1312356cabdb59f9bbbf083eb04ee73435e3c53c
Pallavi Saxena <saxena.pallavi7211@gmail.com>
2022-10-31 15:04:59 +0530

    token stem code added

---
+++ src/chatbot/nltk_utils.py
@@ -0,0 +1,28 @@
+import numpy as np
+import nltk
+nltk.download('punkt')
+from nltk.stem.porter import PorterStemmer
+stemmer = PorterStemmer()
+
+def tokenize(sentence):
+    """
+    split sentence into array of words/tokens
+    a token can be a word or punctuation character, or number
+    """
+    return nltk.word_tokenize(sentence)
+
+
+def stem(word):
+    """
+    stemming = find the root form of the word
+    examples:
+    words = ["organize", "organizes", "organizing"]
+    words = [stem(w) for w in words]
+    -> ["organ", "organ", "organ"]
+    """
+    return stemmer.stem(word.lower())
+
+
+def bag_of_words(tokenized_sentence, words):
+    pass
+
